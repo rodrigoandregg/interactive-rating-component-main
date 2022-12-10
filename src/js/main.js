35c62241dd1819952c1985d27ec768581204096d
Rodrigo Andre Gutierrez <rodrigoandregutierrez@gmail.com>
2022-12-10 18:03:20 -0300

    add styles and scripts

---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+const cardContentRating = document.querySelector("#rating");
+const cardContentThanks = document.querySelector("#thanks");
+const thanksParagraph = document.querySelector(".card__paragraph--thanks");
+const ratingBtn = document.querySelectorAll(".card__btn");
+const submitBtn = document.querySelector(".card__cta");
+let score;
+
+cardContentThanks.style.display = "none";
+
+ratingBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    score = btn.textContent;
+  });
+});
+
+const onSubmit = () => {
+  cardContentThanks.style.display = "flex";
+  cardContentRating.style.display = "none";
+  thanksParagraph.textContent = `You selected ${score} out of 5`;
+};
+
+submitBtn.addEventListener("click", onSubmit);
